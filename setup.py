1db076233d469734165e6043fbdabc08912f7df0
root <root@test1234.clyso.cloud>
2022-08-04 10:08:15 +0200

    convert python3 setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from distutils.core import setup
 
@@ -8,9 +8,8 @@ setup(name='gptools',
       author='Mark Seger',
       author_email='mseger@suse.com',
       url='https://github.com/markseger/getput',
-
       data_files=[('/usr/bin',['getput','gpmulti','gpsuite','gpsum','gpwhere']),
                   ('/etc/gpsuite.d', ['gpsuite.conf']),
                   ('/usr/share/doc/gptools',['getting-started.txt','RELEASE-gptools','Introduction.pdf']),
                   ('/usr/share/man/man1',['getput.1', 'gpmulti.1', 'gpsuite.1', 'gpsum.1', 'gpwhere.1'])]
-     
+      )
